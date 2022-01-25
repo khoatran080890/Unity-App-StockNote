3b7b140633191e466cb6df45d851da4b38782fed
khoatran0808 <khoa.tran0808@gmail.com>
2022-01-25 17:10:13 +0700

    update fire base (not include C:\UNITY\Game Self\Game Developing\Stock Note\Assets\Firebase\Plugins\x86_64\FirebaseCppApp-8_7_0.so)

---
+++ Assets/MAIN GAME/MAIN/Plugins/FireBase/FirebaseManager.cs
@@ -0,0 +1,25 @@
+﻿using Firebase;
+using Firebase.Database;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirebaseManager : MonoBehaviour
+{
+    FirebaseApp app;
+    DatabaseReference databaseInstance;
+    private void Awake()
+    {
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
+            var dependencyStatus = task.Result;
+            if (dependencyStatus == DependencyStatus.Available)
+            {
+                app = FirebaseApp.DefaultInstance;
+            }
+            else
+            {
+                Debug.LogError(System.String.Format("Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+            }
+        });
+    }
+}
